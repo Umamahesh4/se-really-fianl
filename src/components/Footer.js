f6d5873fa5b32ebcfeeb0f18244c4d2dcce38946
Umamahesh4 <umamaheshpalla2004@gmail.com>
2025-04-01 21:14:39 +0530

    change the footer code ak3

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,75 +3,17 @@ import React from "react";
 function Footer() {
   return (
     <footer id="footer" className="footer">
-      {/* About Us Section */}
       <div className="footer-col">
         <h4>About Us</h4>
-        <ul>
-          <li>
-            <a href="/our-story">Our Story</a>
-          </li>
-          <li>
-            <a href="/careers">Careers</a>
-          </li>
-          <li>
-            <a href="/privacy-policy">Privacy Policy</a>
-          </li>
-        </ul>
+        <p>Our Story | Careers | Privacy Policy</p>
       </div>
-
-      {/* Contact Us Section */}
       <div className="footer-col">
         <h4>Contact Us</h4>
-        <ul>
-          <li>
-            <a href="/support">Support</a>
-          </li>
-          <li>
-            <a href="/sales">Sales</a>
-          </li>
-          <li>
-            <a href="/feedback">Feedback</a>
-          </li>
-        </ul>
+        <p>Support | Sales | Feedback</p>
       </div>
-
-      {/* Social Media Links */}
       <div className="footer-col">
         <h4>Follow Us</h4>
-        <div className="social-links">
-          <a
-            href="https://facebook.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Facebook"
-          >
-            <i className="fab fa-facebook-f"></i>
-          </a>
-          <a
-            href="https://twitter.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Twitter"
-          >
-            <i className="fab fa-twitter"></i>
-          </a>
-          <a
-            href="https://instagram.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Instagram"
-          >
-            <i className="fab fa-instagram"></i>
-          </a>
-          <a
-            href="https://linkedin.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="LinkedIn"
-          >
-            <i className="fab fa-linkedin-in"></i>
-          </a>
-        </div>
+        <p>Facebook | Twitter | Instagram | LinkedIn</p>
       </div>
     </footer>
   );
