5c4b6d87f2eeb09f15356c809997ae85d19cc354
Umamahesh4 <umamaheshpalla2004@gmail.com>
2025-04-01 21:03:38 +0530

    change the footer code ak

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,26 +6,34 @@ function Footer() {
       <div className="footer-col">
         <h4>About Us</h4>
         <ul>
-          <li><a href="#">Our Story</a></li>
-          <li><a href="#">Careers</a></li>
-          <li><a href="#">Privacy Policy</a></li>
+          <li><a href="/our-story">Our Story</a></li>
+          <li><a href="/careers">Careers</a></li>
+          <li><a href="/privacy-policy">Privacy Policy</a></li>
         </ul>
       </div>
       <div className="footer-col">
         <h4>Contact Us</h4>
         <ul>
-          <li><a href="#">Support</a></li>
-          <li><a href="#">Sales</a></li>
-          <li><a href="#">Feedback</a></li>
+          <li><a href="/support">Support</a></li>
+          <li><a href="/sales">Sales</a></li>
+          <li><a href="/feedback">Feedback</a></li>
         </ul>
       </div>
       <div className="footer-col">
         <h4>Follow Us</h4>
         <div className="social-links">
-          <a href="#"><i className="fab fa-facebook-f"></i></a>
-          <a href="#"><i className="fab fa-twitter"></i></a>
-          <a href="#"><i className="fab fa-instagram"></i></a>
-          <a href="#"><i className="fab fa-linkedin-in"></i></a>
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-facebook-f"></i>
+          </a>
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-linkedin-in"></i>
+          </a>
         </div>
       </div>
     </footer>
